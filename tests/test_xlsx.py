52556796ad5b078ea6bfc1c48e13e7a9592d129f
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-08-14 19:20:32 +0300

    completed xlsx test

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -1,6 +1,23 @@
+import os
 from openpyxl import load_workbook
+
+
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
-sheet = workbook.active
-print(sheet.cell(row=3, column=2).value)
+XLSX_FILE_PATH = os.path.abspath('resources/file_example_XLSX_50.xlsx')
+PROJECT_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(XLSX_FILE_PATH)))
+JOINED_PATH = os.path.join(
+    PROJECT_ROOT_PATH, 'tests', 'resources', 'file_example_XLSX_50.xlsx'
+)
+
+
+def test_cell_value():
+    workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
+    sheet = workbook.active
+
+    row = 3
+    col = 2
+
+    expected_value = 'Mara'
+
+    assert sheet.cell(row=row, column=col).value == expected_value
