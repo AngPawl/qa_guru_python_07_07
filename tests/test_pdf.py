36c018b474ededf9d1f0a78d972d6252ac3d6b4e
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-08-15 14:42:29 +0300

    pdf test refactoring

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,48 +1,52 @@
 import pypdf
 import os.path
+from .conftest import RESOURCE_ROOT_PATH
 
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-PDF_FILE_PATH = os.path.abspath('resources/docs-pytest-org-en-latest.pdf')
-PROJECT_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(PDF_FILE_PATH)))
-JOINED_PATH = os.path.join(
-    PROJECT_ROOT_PATH, 'tests', 'resources', 'docs-pytest-org-en-latest.pdf'
-)
-
 
 def test_number_of_pages():
-    with open(JOINED_PATH, 'rb') as pdf_file:
+    pdf_file_path = os.path.join(RESOURCE_ROOT_PATH, 'docs-pytest-org-en-latest.pdf')
+    with open(pdf_file_path, 'rb') as pdf_file:
         reader = pypdf.PdfReader(pdf_file)
 
         number_of_pages = len(reader.pages)
 
-        print(number_of_pages)
+        expected_num_of_pages = 412
 
-        assert number_of_pages == 412
+        assert number_of_pages == expected_num_of_pages
 
 
 def test_first_page_text():
-    with open(JOINED_PATH, 'rb') as pdf_file:
+    pdf_file_path = os.path.join(RESOURCE_ROOT_PATH, 'docs-pytest-org-en-latest.pdf')
+    with open(pdf_file_path, 'rb') as pdf_file:
         reader = pypdf.PdfReader(pdf_file)
 
         first_page = reader.pages[0]
 
         text = first_page.extract_text()
 
-        assert (
-            text
-            == "pytest Documentation\nRelease 0.1\nholger krekel, trainer and consultant, https://merlinux.eu/\nJul 14, 2022"
+        expected_first_page_text = (
+            "pytest Documentation\nRelease 0.1\nholger krekel, trainer and consultant, "
+            "https://merlinux.eu/\nJul 14, 2022"
         )
+
+        assert text == expected_first_page_text
 
 
 def test_number_of_images_on_first_page():
-    with open(JOINED_PATH, 'rb') as pdf_file:
+    pdf_file_path = os.path.join(RESOURCE_ROOT_PATH, 'docs-pytest-org-en-latest.pdf')
+    with open(pdf_file_path, 'rb') as pdf_file:
         reader = pypdf.PdfReader(pdf_file)
         first_page = reader.pages[0]
         count = 0
         for image_file in first_page.images:
-            with open(str(count) + image_file.name, 'wb') as fp:
+            with open(
+                os.path.join(RESOURCE_ROOT_PATH, str(count) + image_file.name), 'wb'
+            ) as fp:
                 fp.write(image_file.data)
                 count += 1
 
-    assert count == 1
+    expected_number_of_images = 1
+
+    assert count == expected_number_of_images
