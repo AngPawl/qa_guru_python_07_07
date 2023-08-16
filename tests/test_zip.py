59e53ba06f1713dc9955755046eb196598be4f05
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-08-16 14:26:06 +0300

    refactoring

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -7,28 +7,18 @@ from .conftest import RESOURCE_ROOT_PATH
 
 def test_successful_files_archivation():
     zip_file_path = os.path.join(RESOURCE_ROOT_PATH, 'new_zip.zip')
-    with zipfile.ZipFile(zip_file_path, 'w') as new_zip:
-        path_to_pdf_file = os.path.join(
-            RESOURCE_ROOT_PATH, 'docs-pytest-org-en-latest.pdf'
-        )
-
-        new_zip.write(path_to_pdf_file, basename(path_to_pdf_file))
-
-        path_to_xls_file = os.path.join(RESOURCE_ROOT_PATH, 'file_example_XLS_10.xls')
-
-        new_zip.write(path_to_xls_file, basename(path_to_xls_file))
-
-        path_to_xlsx_file = os.path.join(
-            RESOURCE_ROOT_PATH, 'file_example_XLSX_50.xlsx'
-        )
-
-        new_zip.write(path_to_xlsx_file, basename(path_to_xlsx_file))
 
     files = [
         'docs-pytest-org-en-latest.pdf',
         'file_example_XLS_10.xls',
         'file_example_XLSX_50.xlsx',
     ]
+
+    with zipfile.ZipFile(zip_file_path, 'w') as new_zip:
+        for file in files:
+            path_to_file = os.path.join(RESOURCE_ROOT_PATH, file)
+
+            new_zip.write(path_to_file, basename(path_to_file))
 
     file_names = new_zip.namelist()
 
